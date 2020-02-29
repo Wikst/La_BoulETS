eb6c5899ab2a278ada69fc364cf214cb00ac4675
Jordan <jordan.lam-deschamps.1@etsmtl.net>
2020-02-29 17:00:06 -0500

    Début fenêtre Creation equipe
    Relativement vide. Seul les fichiers associés sont ajoutés. À poursuivre, naturellement.

---
+++ BoulETS/app/src/main/java/com/example/boulets/CreationEquipe.java
@@ -0,0 +1,14 @@
+package com.example.boulets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreationEquipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_equipe);
+    }
+}
