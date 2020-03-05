ef22dcf95bc43191a148489416cade8d7319690d
Jordan <jordan.lam-deschamps.1@etsmtl.net>
2020-03-05 17:38:43 -0500

    Fenêtre confirmation partie(presque ok) (2/2)
    Le Stagging précédent était incomplet. Voici le stagging pour le compléter,

--- BoulETS/app/src/main/java/com/example/boulets/FenetreJeu.java
+++ BoulETS/app/src/main/java/com/example/boulets/FenetreJeu.java
@@ -1,4 +1,13 @@
 package com.example.boulets;
 
-class FenetreJeu {
-}
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FenetreJeu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeu);
+        }
+    }
