baac735580cd011ccaba57ce7906f10f5bb07fc1
Jordan <jordan.lam-deschamps.1@etsmtl.net>
2020-03-04 22:14:12 -0500

    Choix mode (in progress)
    Manque:
    - Liaison des boutons avec les prochaines vues
    - Link la seekbar avec le plain text (nb de mots =2) (Je n'ai pas trouvé de compteur + & - pour changer le nombre. J'ai pris cela comme alternative).

--- BoulETS/app/src/main/java/com/example/boulets/ChoixMode.java
+++ BoulETS/app/src/main/java/com/example/boulets/ChoixMode.java
@@ -3,21 +3,22 @@ package com.example.boulets;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChoixMode extends AppCompatActivity {
-        float x1, x2, y1, y2;
+    float x1, x2, y1, y2;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_choix_mode);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_mode);
+    }
 
     //Pour le swipe
     public boolean onTouchEvent(MotionEvent touchevent) {
-        switch (touchevent.getAction()){
+        switch (touchevent.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 x1 = touchevent.getX();
                 y1 = touchevent.getY();
@@ -25,10 +26,11 @@ public class ChoixMode extends AppCompatActivity {
             case MotionEvent.ACTION_UP:
                 x2 = touchevent.getX();
                 y2 = touchevent.getY();
-                if(x1 < x2){
+                if (x1 < x2) {
                     finish();
                 }
                 break;
         }
         return false;
+    }
 }
