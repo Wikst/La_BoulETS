89d84a2b66a6ac1a09fc24bb56e5ecc8e860f897
Olivier <olivier.gaio@gmail.com>
2020-03-18 11:24:13 -0400

    Modele Jeu est maintenant un Singleton

--- BoulETS/app/src/main/java/Modele/ModeleJeu.java
+++ BoulETS/app/src/main/java/Modele/ModeleJeu.java
@@ -2,6 +2,28 @@ package Modele;
 
 import java.util.List;
 
-public class ModeleJeu {
+//Patern de Singleton inspire de https://openclassrooms.com/forum/sujet/android-meilleur-pattern-singleton
+public final class ModeleJeu {
+
+    private static volatile ModeleJeu instance = null;
+
     private List<Equipe> EquipeList;
+
+    private ModeleJeu(){
+        super();
+    }
+
+    public final static ModeleJeu getInstance() {
+        if (ModeleJeu.instance == null) {
+            // Le mot-clé synchronized sur ce bloc empêche toute instanciation
+            // multiple même par différents "threads".
+            // Il est TRES important.
+            synchronized(ModeleJeu.class) {
+                if (ModeleJeu.instance == null) {
+                    ModeleJeu.instance = new ModeleJeu();
+                }
+            }
+        }
+        return ModeleJeu.instance;
+    }
 }
